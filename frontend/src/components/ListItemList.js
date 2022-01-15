d91b7c9c51526cf6bd54cc8d1b106e47f1b9a3be
m-aljamal <mohammadjamol@gmail.com>
2022-01-15 22:24:57 +0300

    finished books

---
+++ frontend/src/components/ListItemList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useListItem, useListItems } from "utils/list-items";
+import BookRow from "./BookRow";
+const ListItemList = ({
+  user,
+  filterListItem,
+  noListItem,
+  noFilterListItem,
+}) => {
+  const listItems = useListItems(user);
+  const filterListItems = listItems.filter(filterListItem);
+  console.log(listItems);
+  if (!listItems.length) {
+    return <div>{noListItem}</div>;
+  }
+  if (!filterListItems.length) {
+    return <div>{noFilterListItem}</div>;
+  }
+  return (
+    <div>
+      {filterListItems.map((listItem) => (
+        <li key={listItem.id}>
+          <BookRow book={listItem.book} />
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default ListItemList;
--- frontend/src/screen/FinishedBooks.js
+++ frontend/src/screen/FinishedBooks.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import ListItemList from "components/ListItemList";
+import React from "react";
+import { useAuth } from "context/auth-context";
+import { Link } from "react-router-dom";
 const FinishedBooks = () => {
-    return (
-        <div>
-            FinishedBooks
-        </div>
-    )
-}
+  const { user } = useAuth();
+  return (
+    <ListItemList
+      user={user}
+      filterListItem={(li) => Boolean(li.finishDate)}
+      noListItem={
+        <p>
+          Hey there! This is where books will go when you've finished reading
+          them. Get started by heading over to{" "}
+          <Link to="/discover">the Discover page</Link> to add books to your
+          list.
+        </p>
+      }
+      noFilterListItem={
+        <p>
+          Looks like you've got some reading to do! Check them out in your{" "}
+          <Link to="/list">reading list</Link> or
+          <Link to="/discover">discover more</Link>.
+        </p>
+      }
+    />
+  );
+};
 
-export default FinishedBooks
+export default FinishedBooks;
